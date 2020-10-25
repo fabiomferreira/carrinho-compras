75043d98ed97a9173406d90b955eea453ce8c7fd
Fábio Ferreira <fabiomataferreira@gmail.com>
2020-10-25 18:52:33 -0400

    Updates

---
+++ precache-manifest.3ee7dbf65c706d9b30c299f531cbdda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311d7749d58af032ffeaba561ef1f638",
+    "url": "/carrinho-compras/index.html"
+  },
+  {
+    "revision": "23c1e524af01c03e13e4",
+    "url": "/carrinho-compras/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56f07726871baf8fe802",
+    "url": "/carrinho-compras/static/js/2.9780e852.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/carrinho-compras/static/js/2.9780e852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c1e524af01c03e13e4",
+    "url": "/carrinho-compras/static/js/main.627b47fc.chunk.js"
+  },
+  {
+    "revision": "caa84f2e26a8de4c48ea",
+    "url": "/carrinho-compras/static/js/runtime-main.2319d6fa.js"
+  }
+]);
